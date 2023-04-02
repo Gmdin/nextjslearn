1e6644367c9bdea56bea2f2c8f91d4685c6865c6
unknown <gmdin716@gmail.com>
2023-04-02 19:39:21 +0500

    Add img through Image componet by local or ul

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,10 @@
 
-
+import Image from "next/image"
 const home = () => {
   return (
-    <div className="htc">home</div>
+    <div className="htc">home
+    <Image width="400" height="500" src="https://images.pexels.com/photos/14306688/pexels-photo-14306688.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"/>
+    </div>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 
 import Navbar from "../../components/Navbar";
-
+import Image from "next/image";
 const index = () => {
   return (
     <>
       <Navbar/>
+      <Image className="htc" src="/d3.jpg" width="400" height="300"></Image>
     </>
   )
 }
