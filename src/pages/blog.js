295751469bbbd2a1129ed2d811f4afdc3b38dc34
unknown <gmdin716@gmail.com>
2023-04-02 23:06:54 +0500

    fetch data from api by getstaticprops

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import Navbar from '../../components/Navbar';
 
-const blog = () => {
+
+export const getStaticProps=async ()=>{
+  const res=await fetch("https://jsonplaceholder.typicode.com/posts?_start=0&_limit=5");
+  const data=await res.json();
+  return {
+    props:{
+      data
+    }
+  }
+}
+const blog = ({data}) => {
   return (
-    <div>blog</div>
+    <div><Navbar/>
+    <br></br>
+    {data.map((curElem)=>{
+      return(
+        <div key={curElem.id}>
+          <h3>{curElem.id}</h3>
+          <h2>{curElem.title}</h2>
+          </div>
+      )
+    })}
+    </div>
   )
 }
 
